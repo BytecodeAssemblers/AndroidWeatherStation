f411ad01502c55ebfd12d82e983804e9d202c92a
Dimitris <dimitrisbl@hotmail.com>
2019-11-05 18:26:19 +0200

    fetch OpenWeather Data and display
    fetch data from open weather api and diplay in main activity

--- app/src/main/java/com/bytecodeassemblers/androidweatherstation/MainActivity.java
+++ app/src/main/java/com/bytecodeassemblers/androidweatherstation/MainActivity.java
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bytecodeassemblers.androidweatherstation.open_weather.OpenWeatherController;
+
 public class MainActivity extends AppCompatActivity {
+
+    private OpenWeatherController openWeatherController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
+        openWeatherController = new OpenWeatherController(this);
     }
 }
