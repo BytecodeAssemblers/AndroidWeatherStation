e4464a762453bce13a6eb888463287aa0f96249e
Dimitris <dimitrisbl@hotmail.com>
2019-11-18 02:14:03 +0200

    create openWeather_model
    first part for openWeather model

---
+++ app/src/main/java/com/bytecodeassemblers/androidweatherstation/openWeather_model/OpenWeatherCoord.java
@@ -0,0 +1,23 @@
+package com.bytecodeassemblers.androidweatherstation.openWeather_model;
+
+public class OpenWeatherCoord {
+
+    private String lat;
+    private String lon;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+}
---
+++ app/src/main/java/com/bytecodeassemblers/androidweatherstation/openWeather_model/OpenWeatherMain.java
@@ -0,0 +1,51 @@
+package com.bytecodeassemblers.androidweatherstation.openWeather_model;
+
+public class OpenWeatherMain {
+
+    private String temp;
+    private String pressure;
+    private String humidity;
+    private String tempMin;
+    private String tempMax;
+
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getTempMin() {
+        return tempMin;
+    }
+
+    public void setTempMin(String tempMin) {
+        this.tempMin = tempMin;
+    }
+
+    public String getTempMax() {
+        return tempMax;
+    }
+
+    public void setTempMax(String tempMax) {
+        this.tempMax = tempMax;
+    }
+}
